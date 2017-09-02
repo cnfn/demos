c62a1f94454526415ea08b1cd6248125abd02bd7
Cnfn <cnfnss@gmail.com>
2017-09-02 23:26:19 +0800

    JUnit5: mockito.Mock

---
+++ UnitTest/src/test/java/com/example/mockito/TestMockJunit5.java
@@ -0,0 +1,49 @@
+package com.example.mockito;
+
+import java.util.Collections;
+
+import com.example.dao.School;
+import com.example.dao.Teacher;
+import com.example.service.TeacherService;
+import com.example.service.impl.SchoolServiceImpl;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author zhixiao.mzx
+ * @date 2017/09/02
+ */
+@ExtendWith(MockitoExtension.class)
+public class TestMockJunit5 {
+    @InjectMocks
+    private SchoolServiceImpl schoolService;
+
+    @Mock
+    private TeacherService teacherService;
+
+    @Test
+    public void testGetBeforeStub() {
+        School school = schoolService.get("schoolName");
+        assertThat(school.getTeachers()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("返回指定的 teachers")
+    public void testGetAfterStub() {
+        String mockedTeacherName = RandomStringUtils.randomPrint(10);
+        Teacher teacher = new Teacher();
+        teacher.setName(mockedTeacherName);
+        when(teacherService.list(Mockito.anyString())).thenReturn(Collections.singletonList(teacher));
+
+        School school = schoolService.get("schoolName");
+        assertThat(school.getTeachers()).contains(teacher);
+    }
+}
